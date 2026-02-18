f85a056a248b0296f79f25fb0f0d93b4e1478183
Caesar Zeppeli <aventajado.al@obmontessori.edu.ph>
2026-02-18 21:16:29 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+if section == 'Ruby'
+    print ('Red Bulldogs')
+
+elif section == 'Sapphire'
+    print ('Blue Bears')
+
+elif section == 'Topaz'
+    print('Yellow Tigers')
+
+elif section =='Emerald'
+    print('Green Hornets')
